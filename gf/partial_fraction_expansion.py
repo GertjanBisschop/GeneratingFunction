7fd258b58d2231fc3fe068a0a6a5baa04fc04654
Gertjan Bisschop <gertjanbisschop@gmail.com>
2021-08-09 10:58:25 +0100

    numba compiled version of expansion algorithm

--- gf/partial_fraction_expansion.py
+++ gf/partial_fraction_expansion.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numba
 import scipy.linalg
 
 """
@@ -12,12 +13,12 @@ doi: 10.1109/TCSI.2013.2283998.
 """
 
 def return_beta(denominator):
-	return denominator[:,None]-denominator[None,:]
+	return denominator[:,None]-denominator[None,:].astype(np.float64)
 
 def return_binom_coefficients(n):
 	return scipy.linalg.pascal(n)
 
-def return_first_two(A, B, m1, m2, max_multiplicity=None):
+def return_first_two(A, B, m1, m2, max_multiplicity):
 	result = np.zeros((2, max_multiplicity), dtype=np.float64)
 	m1_idxs = np.arange(1, m1+1,dtype=int)
 	m2_idxs = np.arange(1, m2+1,dtype=int)
@@ -45,20 +46,6 @@ def derive_residues(A, B, multiplicities, max_multiplicity):
 
 	return result
 
-def derive_residues_temp(A, B, multiplicities, num_poles):
-	max_multiplicity = np.max(multiplicities)
-	interm_result = np.zeros((num_poles, max_multiplicity))
-	interm_result[0:2] = return_first_two(
-		A,
-		B,
-		multiplicities[0], 
-		multiplicities[1],
-		max_multiplicity
-	)
-	result = single_n_1(A, B, multiplicities, interm_result, num_poles)
-	result[-1] = single_n(A,B, multiplicities, interm_result, num_poles)
-	return result
-
 def derive_residues_next(A, B, multiplicities, intermediate_result, num_poles):
 	result = np.zeros_like(intermediate_result)
 	for i, multiplicity in enumerate(multiplicities[:num_poles-1]):
@@ -78,6 +65,64 @@ def derive_residues_next(A, B, multiplicities, intermediate_result, num_poles):
 				temp+=intermediate_result[i,j] * A[j, multiplicities[num_poles -1] - (L + 1)]/(B[num_poles-1,i]**(j+1))    
 			result[num_poles-1, L]+=temp/beta
 	return result
+
+# numba compiled functions
+@numba.njit(numba.float64[:,:](numba.uint64[:,:], numba.float64[:,:],numba.int64,numba.int64,numba.int64))
+def return_first_two_numba(A, B, m1, m2, max_multiplicity=None):
+	result = np.zeros((2, max_multiplicity), dtype=np.float64)
+	m1_idxs = np.arange(1, m1+1,dtype=np.int8)
+	m2_idxs = np.arange(1, m2+1,dtype=np.int8)
+	leading1 = (-np.ones(m1, dtype=np.int8))**(m1-m1_idxs)
+	leading2 = (-np.ones(m2, dtype=np.int8))**(m2-m2_idxs)
+	c1 = np.zeros(m1, dtype=np.float64)
+    for m1_idx in m1_idxs:
+        c1[m1_idx-1] = A[m2-1,m1-m1_idx]/B[0,1]**(m1+m2-m1_idx)
+    c2 = np.zeros(m2, dtype=np.float64)
+    for m2_idx in m2_idxs:
+        c2[m2_idx-1] = A[m1-1,m2-m2_idx]/B[1,0]**(m1+m2-m2_idx)
+	result[0,:c1.size] = leading1 * c1
+	result[1,:c2.size] = leading2 * c2
+	return result
+
+@numba.njit(numba.float64[:,:](numba.uint64[:,:], numba.float64[:,:], numba.int64[:], numba.int64))
+def derive_residues_numba(A, B, multiplicities, max_multiplicity):
+	#check if there are two poles
+	num_poles = multiplicities.size
+	result = np.zeros((num_poles, max_multiplicity), np.float64)
+	result[0:2] = return_first_two_numba(
+		A,
+		B,
+		multiplicities[0], 
+		multiplicities[1],
+		max_multiplicity
+	)
+	for n in range(3, num_poles+1):
+		result = derive_residues_next_numba(A,B, multiplicities, result, n)
+
+	return result
+
+@numba.njit(numba.float64[:,:](numba.uint64[:,:], numba.float64[:,:],numba.int64[:],numba.float64[:,:],numba.int64))
+def derive_residues_next_numba(A, B, multiplicities, intermediate_result, num_poles):
+	result = np.zeros_like(intermediate_result)
+	for i, multiplicity in enumerate(multiplicities[:num_poles-1]):
+		for L in range(multiplicity):
+			beta = B[i, num_poles-1]**(np.float64(multiplicities[num_poles-1] - (L + 1)))
+			leading_ones = (-1)**(L + 1)
+			temp = 0
+			for j in range(L, multiplicity):
+				temp+=intermediate_result[i,j]*A[multiplicities[num_poles-1]-1, j-L]/(B[num_poles-1,i]**(j+1))
+			result[i,L] = leading_ones * temp / beta
+	
+	for L in range(multiplicities[num_poles-1]):
+		for i in range(num_poles-1):
+			temp = 0
+			beta = B[i, num_poles-1]**(np.float64(multiplicities[num_poles-1] - (L + 1)))
+			for j in range(multiplicities[i]):
+				temp+=intermediate_result[i,j] * A[j, multiplicities[num_poles -1] - (L + 1)]/(B[num_poles-1,i]**(j+1))    
+			result[num_poles-1, L]+=temp/beta
+	return result
+
+# single step functions
 
 def single_n_1(A, B, multiplicities, intermediate_result, num_poles):
 	results = np.zeros_like(intermediate_result)
